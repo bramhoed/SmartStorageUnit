49d9033965e159a1df3954ed834e8b13ebb2ac0b
CptFrikadel <adkupper@gmail.com>
2020-03-07 23:40:03 +0100

    Moved event handlers to own file

---
+++ EventHandlers.py
@@ -0,0 +1,14 @@
+
+
+# Definition of the event handlers
+def onCuttingVolume(sender, event):
+    print('Cutting volume event!')
+
+def onSoundSpectrum(sender, event):
+    print('Sound Spectrum event!')
+
+def onBoardWeight(sender, event):
+    print('Board Weight event!')
+
+def onCuttingSpeed(sender, event):
+    print('Cutting Speed event!')
--- main.py
+++ main.py
@@ -2,20 +2,8 @@
 
 from oocsi import OOCSI
 from OOCSIListener import Listener
+from  EventHandlers import *
 
-# Definition of the event handlers
-
-def onCuttingVolume(sender, event):
-    print('Cutting volume event!')
-
-def onSoundSpectrum(sender, event):
-    print('Sound Spectrum event!')
-
-def onBoardWeight(sender, event):
-    print('Board Weight event!')
-
-def onCuttingSpeed(sender, event):
-    print('Cutting Speed event!')
 
 
 receiver_channels = {'cuttingVolumeChannel': onCuttingVolume,
