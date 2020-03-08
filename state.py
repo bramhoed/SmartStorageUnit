e1d80f7ca7a2db6401a23ad22b8baef8bf7791a3
CptFrikadel <adkupper@gmail.com>
2020-03-08 23:31:13 +0100

    Added global state class

---
+++ state.py
@@ -0,0 +1,19 @@
+from recipe import Recipe
+
+class StorageUnitState:
+    """
+    Class to represent the global state of the storage unit
+
+    Attributes:
+
+    oocsi (OOCSI): the oocsi network handle
+    items (dict): Items currently stored in the unit with amounts
+    curr_recipe (Recipe): The active recipe
+    """
+    
+    def __init__(self, _oocsi):
+        self.oocsi = _oocsi
+        self.items = {}
+        self.curr_recipe = Recipe('example_recipe.json')
+
+        
