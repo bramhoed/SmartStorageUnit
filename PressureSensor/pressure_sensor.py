c906d8cc0da3a0dd6648a12b19228013cbde568b
CptFrikadel <adkupper@gmail.com>
2020-03-08 20:22:35 +0100

    Start of pressure sensor implementation

---
+++ PressureSensor/pressure_sensor.py
@@ -0,0 +1,20 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(21, GPIO.IN)
+
+prev_input = 0
+
+while True:
+    input = GPIO.input(21)
+
+    if ((not prev_input) and input):
+        print("Wollah pressure G!")
+
+    prev_input = input
+
+    time.sleep(0.10)
+
+finally:
+    GPIO.cleanup()
