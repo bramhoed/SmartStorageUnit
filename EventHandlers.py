91b7135ed9696aab77badbae9c70a27fd3591894
CptFrikadel <adkupper@gmail.com>
2020-03-08 23:31:42 +0100

    Moved Handlers into class

--- EventHandlers.py
+++ EventHandlers.py
@@ -1,14 +1,36 @@
 
+class EventHandler:
+    """
+    Wrapper class for the event handlers. 
 
-# Definition of the event handlers
-def onCuttingVolume(sender, event):
-    print('Cutting volume event!')
+    Attributes:
+    user_cutting (bool): Wether or not the user is cutting
 
-def onSoundSpectrum(sender, event):
-    print('Sound Spectrum event!')
+    
+    """
 
-def onBoardWeight(sender, event):
-    print('Board Weight event!')
+    def __init__(self, _global_state):
+        self.global_state = _global_state
+        self.user_cutting = False
 
-def onCuttingSpeed(sender, event):
-    print('Cutting Speed event!')
+    def onCuttingVolume(self, sender, event):
+
+        if ((int(event['volume']) > 0) and (not self.user_cutting)):
+
+            print('User is cutting something!')
+            self.user_cutting = True
+
+        elif((int(event['volume']) <= 0) and (self.user_cutting)):
+
+            print('User stopped cutting something!')
+            self.user_cutting = False
+
+            
+    def onSoundSpectrum(self, sender, event):
+        print('Sound Spectrum event!')
+
+    def onBoardWeight(self, sender, event):
+        print('Board Weight event!')
+
+    def onCuttingSpeed(self, sender, event):
+        print('Cutting Speed event!')
